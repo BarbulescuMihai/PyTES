fb9293ca4630fdd9739cb13c018bc1b6e3ffd6f0
mihai <mihai_barbulescu@hotmail.com>
2017-11-08 16:49:02 +0000

    Overhaul of grid_solver. Added some docstrings and comments.

--- main.py
+++ main.py
@@ -1,84 +1,130 @@
+"""
+Module docstring.
+"""
+
 from functools import partial
+import timeit
 import numpy as np
 import scipy.optimize as sp
 import mpmath as mp
-import timeit
-from numbers import Number
 
 def grid_solver(func,
-                x_range, y_range,
-                args=(None),
-                method='sp.newton',
-                debug=True):
+                x_range,
+                y_range,
+                axes,
+                args,
+                method='sp.newton'):
+    """
+    Finds the roots of func in a box defined by x_range and y_range.
+
+    Parameters
+    ----------
+        func: function
+            A function of an arbitrary number of variables.
+        x_range: array_like
+            An array defining the x-axis.
+        y_range: array_like
+            An array defining the y-axis.
+        axes: dictionary
+            Specify which axis corresponds to which variable in func.
+            This must have the form {'x_axis':'var1', 'y_axis':var2}, where var1 and var2 are
+            arguments of func.
+        args: dictionary
+            Specify any other arguments of func as keys and their corresponding values.
+            These must be entered as {'key1':value1, 'key2':value2, etc.}
+        method: string
+            Specify which solver should be used to find the points.
+            Currently supported: sp.newton, sp.brentq, sp.brenth.
+
+    Returns
+    -------
+        points: array_like
+            A 2 column numpy array.
+            The entries in column 0 are points on the x-axis.
+            The entries in column 1 are points on the y-axis.
+    """
 
     points = []
 
-    if debug:
-        start_time = timeit.default_timer()
+    start_time = timeit.default_timer()
 
     if method == 'sp.newton':
 
         for x_loc in x_range:
 
-            if type(args)==tuple:
-                arguments = [x_loc if i is None else i for i in np.array(args)]
-            elif isinstance(x_range[2], Number):
-                arguments = [x_loc]
-            else:
-                print('Invalid arguments.\
-                      \nWas expecting number or tuple.\
-                      \nInstead received {}'.format(type(args)))
-                break
+            """
+            Adds x_loc in args and assigns it to the variable corresponding to x_axis.
+            Creates a partial function using all the extra arguments.
+            """
+            args[axes['x_axis']] = x_loc
+            func_part = partial(func, **args)
 
             for y_loc in y_range:
                 try:
-                    root = sp.newton(func, y_loc, args=tuple(arguments), tol=1e-20)
+                    """
+                    Attempts to find a root of the function for the specified y_loc.
+                    """
+
+                    root = sp.newton(func_part, y_loc, tol=1e-20)
+
+                    """
+                    Makes sure that the root found is within y_range.
+                    If it is within 1e-6 of any other root it is discarded.
+                    If the imaginary part is less than 1e-5 it is discarded.
+                    """
+
                     if (root > y_range[0] and root < y_range[-1]) \
                     and not np.isclose(root, points, atol=1e-6).any():
+
                         if np.imag(root) != 0 and np.imag(root) < 1e-5:
                             root = np.real(root)
+
                         points.append([x_loc, root])
+
+                    """
+                    If a root is not found, the method returns returns a RuntimeError.
+                    The function then passes to the next value of y_loc.
+                    """
+
                 except RuntimeError:
                     pass
 
     if method == 'sp.brentq':
 
-        """Half the stepsize used in y_range."""
+        """
+        Half the stepsize used in y_range.
+        """
         step_size = np.abs(y_range[0]-y_range[-1])/(2*len(y_range))
 
         for x_loc in x_range:
 
-            if type(args)==tuple:
-                arguments = [x_loc if i is None else i for i in np.array(args)]
-            elif isinstance(x_range[2], Number):
-                arguments = [x_loc]
-            else:
-                print('Invalid arguments.\
-                      \nWas expecting number or tuple.\
-                      \nInstead received {}'.format(type(args)))
-                break
+            """
+            Adds x_loc in args and assigns it to the variable corresponding to x_axis.
+            Creates a partial function using all the extra arguments.
+            """
+            args[axes['x_axis']] = x_loc
+            func_part = partial(func, **args)
 
             for y_loc in y_range:
 
                 y_range_local = np.linspace(y_loc-step_size, y_loc+step_size, 10000)
 
-                flip_func = partial(lambda *args: func(*args[::-1]), *arguments[::-1])
-
-                func_grid = flip_func(y_range_local)
+                func_grid = func_part(y_range_local)
 
                 grid_shift_up = func_grid[1:]
                 grid_shift_down = func_grid[:-1]
                 grid_prod = np.real(grid_shift_down) * np.real(grid_shift_up)
                 root_locs = (grid_prod < 0) * (np.abs(func_grid[:-1]) < 1)
 
-                for i in np.where(root_locs == True)[0]:
+                for i in np.where(root_locs)[0]:
                     brent_start = y_range_local[i-2]
                     brent_end = y_range_local[i+2]
 
                     try:
-                        root = sp.brentq(func, brent_start, brent_end, args=tuple(arguments),
+                        root = sp.brentq(func_part, brent_start, brent_end,
                                          xtol=1e-5, rtol=1e-5, maxiter=200)
-                        if (root > y_range[0] and root < y_range[-1]) and not np.isclose(root, points, atol=1e-6).any():
+                        if (root > y_range[0] and root < y_range[-1]) \
+                        and not np.isclose(root, points, atol=1e-6).any():
                             if np.imag(root) != 0 and np.imag(root) < 1e-5:
                                 root = np.real(root)
                             points.append([x_loc, root])
@@ -88,41 +134,40 @@ def grid_solver(func,
 
     if method == 'sp.brenth':
 
+        """
+        Half the stepsize used in y_range.
+        """
         step_size = np.abs(y_range[0]-y_range[-1])/(2*len(y_range))
 
         for x_loc in x_range:
 
-            if type(args)==tuple:
-                arguments = [x_loc if i is None else i for i in np.array(args)]
-            elif isinstance(x_range[2], Number):
-                arguments = [x_loc]
-            else:
-                print('Invalid arguments.\
-                      \nWas expecting number or tuple.\
-                      \nInstead received {}'.format(type(args)))
-                break
+            """
+            Adds x_loc in args and assigns it to the variable corresponding to x_axis.
+            Creates a partial function using all the extra arguments.
+            """
+            args[axes['x_axis']] = x_loc
+            func_part = partial(func, **args)
 
             for y_loc in y_range:
 
                 y_range_local = np.linspace(y_loc-step_size, y_loc+step_size, 10000)
 
-                flip_func = partial(lambda *args: func(*args[::-1]), *arguments[::-1])
-
-                func_grid = flip_func(y_range_local)
+                func_grid = func_part(y_range_local)
 
                 grid_shift_up = func_grid[1:]
                 grid_shift_down = func_grid[:-1]
                 grid_prod = np.real(grid_shift_down) * np.real(grid_shift_up)
                 root_locs = (grid_prod < 0) * (np.abs(func_grid[:-1]) < 1)
 
-                for i in np.where(root_locs == True)[0]:
+                for i in np.where(root_locs)[0]:
                     brent_start = y_range_local[i-2]
                     brent_end = y_range_local[i+2]
 
                     try:
-                        root = sp.brenth(func, brent_start, brent_end, args=tuple(arguments),
+                        root = sp.brenth(func_part, brent_start, brent_end,
                                          xtol=1e-5, rtol=1e-5, maxiter=200)
-                        if (root > y_range[0] and root < y_range[-1]) and not np.isclose(root, points, atol=1e-6).any():
+                        if (root > y_range[0] and root < y_range[-1]) \
+                        and not np.isclose(root, points, atol=1e-6).any():
                             if np.imag(root) != 0 and np.imag(root) < 1e-5:
                                 root = np.real(root)
                             points.append([x_loc, root])
@@ -132,27 +177,28 @@ def grid_solver(func,
 
     points = np.array(points)
 
-    if debug:
+    end_time = timeit.default_timer()
+    running_time = end_time - start_time
 
-        end_time = timeit.default_timer()
-        running_time = end_time - start_time
-
-        print('Grid solver finished running.\
-              \nUsing the {} method, the total running time was {:.6f}s.\
-              \nA total of {} real roots, and {} complex roots were found in the {}x{} grid.'
-              .format(method,
-                      running_time,
-                      np.count_nonzero(np.isreal(points[:,1])),
-                      np.count_nonzero(np.isreal(points[:,1])==False),
-                      len(x_range), len(y_range)
-                      )
-              )
+    print('\n' + '='*60 + '\n',
+          '\nGrid solver finished running.\
+          \nUsing the {} method, the total running time was {:.6f}s.\
+          \nA total of {} real roots, and {} complex roots were found in the {}x{} grid.\n'
+          .format(method,
+                  running_time,
+                  np.count_nonzero(np.isreal(points[:, 1])),
+                  np.count_nonzero(np.isreal(points[:, 1]) is False),
+                  len(x_range), len(y_range)
+                 ),
+          '\n' + '='*60 + '\n'
+         )
 
     return points
 
 def find_sign_change(func, x_range, y_range, args=(None)):
-
-    """"Not really functional. Needs argument system implemented."""
+    """"
+    Not really functional. Needs argument system implemented.
+    """
 
     x_grid, y_grid = np.meshgrid(x_range, y_range)
     func_grid = func(y_grid, x_grid, args)
@@ -161,12 +207,15 @@ def find_sign_change(func, x_range, y_range, args=(None)):
     grid_shift_down = func_grid[:-1, :]
     grid_prod = np.real(grid_shift_down) * np.real(grid_shift_up)
 
-    root_locs = x_grid[(grid_prod < 0) * (func_grid[:-1,:] < 10)]
-    roots = y_grid[(grid_prod < 0) * (func_grid[:-1,:] < 10)]
+    root_locs = x_grid[(grid_prod < 0) * (func_grid[:-1, :] < 10)]
+    roots = y_grid[(grid_prod < 0) * (func_grid[:-1, :] < 10)]
 
     return root_locs, roots
 
 def find_first_imag(func, x_range, y_range, args=(None)):
+    """
+    Docstring here.
+    """
 
     for x_loc in x_range:
 
@@ -180,8 +229,10 @@ def find_first_imag(func, x_range, y_range, args=(None)):
             except RuntimeError:
                 pass
 
-def next_root(func, x_loc, y_loc, step_size, arguments, points,
-                 wordy=True):
+def next_root(func, x_loc, y_loc, step_size, arguments, points):
+    """
+    Docstring here.
+    """
 
     jump_limit = 0.1
     iterations = 200
@@ -224,6 +275,9 @@ def next_root(func, x_loc, y_loc, step_size, arguments, points,
 
 def line_trace_sp(func, x_loc, y_loc, step_size, x_end_left, x_end_right,
                   args=None, wordy=True, func_mp=None, solver='halley'):
+    """
+    Docstring here.
+    """
 
     points = np.zeros((1, 2))
     step_init = step_size
@@ -235,7 +289,7 @@ def line_trace_sp(func, x_loc, y_loc, step_size, x_end_left, x_end_right,
 
         try:
             points, step_size, x_error, x_loc = next_root(func, x_loc, y_loc, -step_size,
-                                                             tuple(arguments), points, wordy)
+                                                          tuple(arguments), points)
 
         except (RuntimeError, ValueError) as err:
             if step_size >= step_init * 2**(-5):
@@ -272,7 +326,7 @@ def line_trace_sp(func, x_loc, y_loc, step_size, x_end_left, x_end_right,
 
         try:
             points, step_size, x_error, x_loc = next_root(func, x_loc, y_loc, step_size,
-                                                             tuple(arguments), points, wordy)
+                                                          tuple(arguments), points)
         except (RuntimeError, ValueError) as err:
             if wordy:
                 print('Error when solving for x = {:.5f}.\n Error message:{}'
@@ -298,17 +352,20 @@ def line_trace_sp(func, x_loc, y_loc, step_size, x_end_left, x_end_right,
     return points
 
 def next_root_mp(func, x_loc, y_loc, step_size, points,
-                 solver='halley', wordy=True, tol=1e-15):
+                 solver='halley', tol=1e-15):
+    """
+    Docstring here.
+    """
 
     jump_limit = 0.1
     maxsteps = 200
 
     try:
         grad = (
-                (points[-1, 1] - points[-2, 1]) +
-                (points[-1, 1] - 2*points[-2, 1] + points[-3, 1])) * \
-                np.abs(step_size/(points[-1, 0] - points[-2, 0])
-                )
+            (points[-1, 1] - points[-2, 1]) +
+            (points[-1, 1] - 2*points[-2, 1] + points[-3, 1])) * \
+            np.abs(step_size/(points[-1, 0] - points[-2, 0])
+                  )
         root_mp = mp.findroot(func, points[-1, 1] + grad, solver, tol)
         root = float(mp.re(root_mp)) + 1j * float(mp.im(root_mp))
         if np.abs(root-points[-1, 1]) < 0.1:
