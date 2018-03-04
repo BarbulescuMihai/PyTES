d1ba2880f6a81a3e63ca88b6eb864648bc985872
Mihai Barbulescu <mihai_barbulescu@hotmail.com>
2018-03-04 11:24:01 +0000

    typo

--- test_class_slab.py
+++ test_class_slab.py
@@ -8,7 +8,7 @@ Created on Sat Jan 7 18:03:02 2017
 import scipy as sc
 
 """
-vphe define
+We define
 
 R1 = rho_1/rho_0
 R2 = rho_2/rho_0
@@ -40,6 +40,7 @@ class Asymmetric_slab:
     def m0(self, vph, M_A):
         return sc.sqrt((1 - (vph - M_A)**2) * (self.c0**2 - (vph - M_A)**2) / ((1 + self.c0**2) * (self.cT**2 - (vph - M_A)**2)))
 
+    #function to be solved
     def disp_rel(self, vph, kx0, M_A):
         return self.m0(vph, M_A)**2 * vph**4 + 1/self.R1 * 1/self.R2 * self.m1(vph) * self.m2(vph) * (1 - (vph - M_A)**2)**2 - \
 	           0.5 * vph**2 * self.m0(vph, M_A) * (1 - (vph - M_A)**2) * (1/self.R1 * self.m1(vph) + 1/self.R2 * self.m2(vph)) * \
